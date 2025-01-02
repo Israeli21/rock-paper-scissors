bc6c10b7866909340043df2ba82b9f41612f2524
Israel Trejo <isrtrejo07@gmail.com>
2025-01-01 23:09:59 -0600

    Added feature where you can press a button and you can activate rock, paper, scissors or the auto functions

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -11,7 +11,7 @@ let intervalID;
 
 function autoPlay(){
   if(!isAutoPlaying){
-    intervalID = setInterval(function(){
+    intervalID = setInterval(() =>{
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
@@ -21,6 +21,42 @@ function autoPlay(){
     isAutoPlaying = false;
   }
 }
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.querySelector('.js-reset-score').addEventListener('click', () => {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElement();
+});
+
+document.querySelector('.js-auto-play').addEventListener('click', () => {
+  autoPlay();
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if(event.key === 'r'){
+    playGame('rock');
+  } else if(event.key === 'p'){
+    playGame('paper');
+  } else if(event.key === 's'){
+    playGame('scissors');
+  } else if(event.key === 'a'){
+    autoPlay();
+  }
+});
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
